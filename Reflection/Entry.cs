b034c84c740abba6d04db5097f8964ee91d4719f
Christen Sebastian <Sebastian.Christen@swisscom.com>
2024-06-13 15:10:47 +0200

    feat( #28 ): read entries from JSON file

---
+++ Reflection/Entry.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Reflection
+{
+    public class Entry
+    {
+        public string Date { get; set; } = DateOnly.FromDateTime(DateTime.Now).ToString(CultureInfo.CurrentCulture);
+        public string DoneToday { get; set; } = "";
+        public string DoneWell { get; set; } = "";
+        public string DoneBad { get; set; } = "";
+        public string Improvements { get; set; } = "";
+        public int StarScale { get; set; } = 0;
+    }
+}
